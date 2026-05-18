fb677d31176482853bd7cbe61ac1e4f611ee1904
Mohamed-Arif-11 <mdariff011@gmail.com>
2026-05-18 13:31:51 +0530

    fixed production cors and api

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -3,7 +3,8 @@ import axios from "axios";
 const API = axios.create({
 
   baseURL:
-    "http://localhost:5000/api",
+    process.env.REACT_APP_API_URL ||
+    "https://kickoff-gzmc.onrender.com/api",
 
 });
 
